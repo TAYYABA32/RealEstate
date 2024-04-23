3f97812f5c22e62bc1a1f37ca4d867c6b0a262bd
Tayyaba <tayyabahuma12@gmail.com>
2024-04-23 22:18:58 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Header from "./Components/Header/Header";
+import Hero from "./Components/Hero/Hero";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Hero />
+    </div>
+  );
+}
+
+export default App;
